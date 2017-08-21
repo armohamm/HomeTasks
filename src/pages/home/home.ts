e3dfeeda322c14fb8cabe77c02a0c2d9e9f6681e
Pedro Henrique de Miranda Arthur <pedro@aceelectronix.com>
2017-08-21 16:22:46 -0700

    Adding some layouts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,39 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
 
   }
-
+  
+ showPrompt() {
+    let prompt = this.alertCtrl.create({
+      title: 'Adding new Task',
+      message: "Enter a name and deadline for this task that you're adding",
+      inputs: [
+        {
+          name: 'Title',
+          placeholder: 'Title'
+        },
+		{
+          name: 'Deadline',
+          placeholder: 'Deadline'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Add',
+          handler: data => {
+            console.log('Add clicked');
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
 }
